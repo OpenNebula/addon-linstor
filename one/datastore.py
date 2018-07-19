e6106628551427f802d710bf31663d0d878b0b6a
Hayley Swimelar <hayley@linbit.com>
2018-07-19 13:31:47 -0700

    add in datastore classes

--- one/datastore.py
+++ one/datastore.py
@@ -19,27 +19,51 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 import xml.etree.ElementTree as ET
 
 
-class Image(object):
+class Datastore(object):
 
-    """Docstring for vm. """
+    """Docstring for Datastore. """
 
     def __init__(self, xml):
         root = ET.fromstring(xml)
         self._ID = root.find("ID").text
-        self._size = root.find("SIZE").text
-        self._datastore_ID = root.find("DATASTORE_ID").text
+        self._name = root.find("NAME").text
+        self._ds_mad = root.find("DS_MAD").text
+        self._tm_mad = root.find("TM_MAD").text
+        self._total_mb = root.find("TOTAL_MB").text
+        self._free_mb = root.find("FREE_MB").text
+        self._used_mb = root.find("USED_MB").text
 
     @property
     def ID(self):
-        """Returns name"""
+        """Returns ID"""
         return self._ID
 
     @property
-    def size(self):
+    def name(self):
         """Returns name"""
-        return self._size
+        return self._name
 
     @property
-    def datastore_ID(self):
-        """Returns name"""
-        return self._datastore_ID
+    def ds_mad(self):
+        """Returns ds_mad"""
+        return self._ds_mad
+
+    @property
+    def tm_mad(self):
+        """Returns tm_mad"""
+        return self._tm_mad
+
+    @property
+    def total_mb(self):
+        """Returns total_mb"""
+        return self._total_mb
+
+    @property
+    def free_mb(self):
+        """Returns free_mb"""
+        return self._free_mb
+
+    @property
+    def used_mb(self):
+        """Returns used_mb"""
+        return self._used_mb
---
+++ one/tests/datastore_test.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+Linstor addon for OpenNebula
+Copyright © 2018 LINBIT USA, LLC
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+"""
+from one import datastore
+
+TEST_XML_1 = """
+<DATASTORE>
+   <ID>1</ID>
+   <UID>0</UID>
+   <GID>0</GID>
+   <UNAME>oneadmin</UNAME>
+   <GNAME>oneadmin</GNAME>
+   <NAME>default</NAME>
+   <PERMISSIONS>
+      <OWNER_U>1</OWNER_U>
+      <OWNER_M>1</OWNER_M>
+      <OWNER_A>0</OWNER_A>
+      <GROUP_U>1</GROUP_U>
+      <GROUP_M>0</GROUP_M>
+      <GROUP_A>0</GROUP_A>
+      <OTHER_U>1</OTHER_U>
+      <OTHER_M>0</OTHER_M>
+      <OTHER_A>0</OTHER_A>
+   </PERMISSIONS>
+   <DS_MAD>fs</DS_MAD>
+   <TM_MAD>shared</TM_MAD>
+   <TYPE>0</TYPE>
+   <DISK_TYPE>0</DISK_TYPE>
+   <CLUSTER_ID>-1</CLUSTER_ID>
+   <CLUSTER />
+   <TOTAL_MB>86845</TOTAL_MB>
+   <FREE_MB>20777</FREE_MB>
+   <USED_MB>1000</USED_MB>
+   <IMAGES />
+   <TEMPLATE>
+      <CLONE_TARGET><![CDATA[SYSTEM]]></CLONE_TARGET>
+      <DISK_TYPE><![CDATA[FILE]]></DISK_TYPE>
+      <DS_MAD><![CDATA[fs]]></DS_MAD>
+      <LN_TARGET><![CDATA[NONE]]></LN_TARGET>
+      <TM_MAD><![CDATA[shared]]></TM_MAD>
+      <TYPE><![CDATA[IMAGE_DS]]></TYPE>
+   </TEMPLATE>
+</DATASTORE>
+"""
+
+TEST_XML_2 = """
+<DATASTORE>
+   <ID>100</ID>
+   <UID>0</UID>
+   <GID>0</GID>
+   <UNAME>steve</UNAME>
+   <GNAME>steve</GNAME>
+   <NAME>fantastic-datastore</NAME>
+   <PERMISSIONS>
+      <OWNER_U>1</OWNER_U>
+      <OWNER_M>1</OWNER_M>
+      <OWNER_A>0</OWNER_A>
+      <GROUP_U>1</GROUP_U>
+      <GROUP_M>0</GROUP_M>
+      <GROUP_A>0</GROUP_A>
+      <OTHER_U>1</OTHER_U>
+      <OTHER_M>0</OTHER_M>
+      <OTHER_A>0</OTHER_A>
+   </PERMISSIONS>
+   <DS_MAD>linstor</DS_MAD>
+   <TM_MAD>shared</TM_MAD>
+   <TYPE>3</TYPE>
+   <DISK_TYPE>0</DISK_TYPE>
+   <CLUSTER_ID>-1</CLUSTER_ID>
+   <CLUSTER />
+   <TOTAL_MB>555555585</TOTAL_MB>
+   <FREE_MB>20000</FREE_MB>
+   <USED_MB>5000</USED_MB>
+   <IMAGES />
+   <TEMPLATE>
+      <CLONE_TARGET><![CDATA[SYSTEM]]></CLONE_TARGET>
+      <DISK_TYPE><![CDATA[FILE]]></DISK_TYPE>
+      <DS_MAD><![CDATA[fs]]></DS_MAD>
+      <LN_TARGET><![CDATA[NONE]]></LN_TARGET>
+      <TM_MAD><![CDATA[shared]]></TM_MAD>
+      <TYPE><![CDATA[IMAGE_DS]]></TYPE>
+   </TEMPLATE>
+</DATASTORE>
+"""
+
+
+def test_create_datastore():
+    test_datastore = datastore.Datastore(TEST_XML_1)
+
+    assert test_datastore.ID == "1"
+    assert test_datastore.name == "default"
+    assert test_datastore.ds_mad == "fs"
+    assert test_datastore.tm_mad == "shared"
+    assert test_datastore.total_mb == "86845"
+    assert test_datastore.free_mb == "20777"
+    assert test_datastore.used_mb == "1000"
+
+    test_datastore = datastore.Datastore(TEST_XML_2)
+
+    assert test_datastore.ID == "100"
+    assert test_datastore.name == "fantastic-datastore"
+    assert test_datastore.ds_mad == "linstor"
+    assert test_datastore.tm_mad == "shared"
+    assert test_datastore.total_mb == "555555585"
+    assert test_datastore.free_mb == "20000"
+    assert test_datastore.used_mb == "5000"
