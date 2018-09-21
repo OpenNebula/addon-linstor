68a30a968b76105808666428678abb81ec2e89f4
Hayley Swimelar <hayley@linbit.com>
2018-09-21 15:20:54 -0700

    don't log during migrate_other calls

--- one/util.py
+++ one/util.py
@@ -55,8 +55,9 @@ def log_info(msg):
     syslog.syslog(syslog.LOG_INFO, "INFO {}".format(msg))
 
 
-def _wait_for_subp(cmd):
-    log_info("running shell command: {}".format(" ".join(cmd)))
+def _wait_for_subp(cmd, log=True):
+    if log:
+        log_info("running shell command: {}".format(" ".join(cmd)))
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _, err = proc.communicate()
 
@@ -120,7 +121,9 @@ def arg_path(string_args):
 
 
 def migrate_other(string_args):
-    return _wait_for_subp(_source(TM_COMMON, "migrate_other", string_args))
+    # We're turning off logging here because this gets called with a huge
+    # base64 image dump and it's too noisy.
+    return _wait_for_subp(_source(TM_COMMON, "migrate_other", string_args), log=False)
 
 
 def show_vm(vm_ID):
